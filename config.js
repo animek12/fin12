a5b99e083e2360a4759b2a7ca8b88b66b2f25232
animek12 <111511065+animek12@users.noreply.github.com>
2022-09-18 13:24:49 +0800

    Update config.js

--- config.js
+++ config.js
@@ -18,9 +18,9 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
+global.owner = ['6282261065144','6282261065144','6282261065144','6282261065144']
+global.premium = ['6282261065144']
+global.packname = 'Fin-MD'
 global.author = 'WhatsApp Bot'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
